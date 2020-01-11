6eced7804efdfdb415f5911f333a106211ad5061
Naoto Mukai <nmukai@sugiyama-u.ac.jp>
2020-01-11 15:00:23 +0900

    cgi

--- cgi-bin/sql.py
+++ cgi-bin/sql.py
@@ -1,9 +1,21 @@
 #! /anaconda3/bin/python
 
+import sqlite3
+
 print ("Content-type:text/html\n\n")
 print ("<html>")
 print ("<body>")
 print("<h1>SQLite on Web</h1>")
-print ("</body>")
-print ("</html>")
 
+DB_FILE = "db/students.db"
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+sql = "select * from students;"
+
+for r in cursor.execute(sql):
+    print("<p>" + str(r) + "</r>")
+
+print ("</body>")
+print ("</html>")    
