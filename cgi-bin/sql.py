192fd3a7f9e8c33d14beb14af515248ef4c87ab8
Naoto Mukai <nmukai@sugiyama-u.ac.jp>
2020-01-16 10:45:21 +0900

    cgi

--- cgi-bin/sql.py
+++ cgi-bin/sql.py
@@ -1,6 +1,7 @@
 #! /anaconda3/bin/python
 
 import sqlite3
+import cgi
 
 print ("Content-type:text/html\n\n")
 print ("<html>")
@@ -12,10 +13,21 @@ DB_FILE = "db/students.db"
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
-sql = "select * from students;"
+# SQL文
+sql = "select * from students "
 
-for r in cursor.execute(sql):
-    print("<p>" + str(r) + "</r>")
+# CGIの処理
+form = cgi.FieldStorage()
+
+if "id" in form:
+    print("<p>ID: " + form["id"].value + "</p>")
+    sql += "where id=" + form["id"].value + ";"
+else:
+    sql += ";"
+
+# SQLの実行
+for record in cursor.execute(sql):
+    print("<p>" + str(record) + "</r>")
 
 print ("</body>")
 print ("</html>")    
--- server.py
+++ server.py
@@ -6,5 +6,10 @@ class Handler(CGIHTTPRequestHandler):
 PORT = 8080
 HOST = "127.0.0.1"
 
+print("http://localhost:8080/cgi-bin/sql.py")
+
+print("http://localhost:8080/cgi-bin/sql.py?id=A001")
+
 httpd = HTTPServer((HOST, PORT), Handler)
 httpd.serve_forever()
+
