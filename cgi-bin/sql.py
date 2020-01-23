01dcd27836030b7d6dd775c86ceb02654c2ee781
Naoto Mukai <nmukai@sugiyama-u.ac.jp>
2020-01-23 14:19:17 +0900

    modify

--- cgi-bin/sql.py
+++ cgi-bin/sql.py
@@ -8,24 +8,30 @@ print ("<html>")
 print ("<body>")
 print("<h1>SQLite on Web</h1>")
 
+# データベース・ファイル
 DB_FILE = "db/students.db"
 
+# データベースへの接続
 connection = sqlite3.connect(DB_FILE)
+
+# カーソルの取得
 cursor = connection.cursor()
 
-# SQL文
-sql = "select * from students "
-
-# CGIの処理
+# フォームの取得
 form = cgi.FieldStorage()
 
+# SQL文の開始
+sql = "select * from students"
+
+# IDによる検索
 if "id" in form:
-    print("<p>ID: " + form["id"].value + "</p>")
-    sql += "where id=" + form["id"].value + ";"
-else:
-    sql += ";"
+    sql += " where id=\"" + form["id"].value + "\""
+
+# SQL文の終了
+sql += ";"
 
 # SQLの実行
+print("<h3>SQL: " + sql + "</h3>")
 for record in cursor.execute(sql):
     print("<p>" + str(record) + "</r>")
 
