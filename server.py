fae730bf5d0e670e609b9601b5a90bc211daf140
Naoto Mukai <nmukai@sugiyama-u.ac.jp>
2020-01-23 14:29:10 +0900

    modify

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
 class Handler(CGIHTTPRequestHandler):
+    # CGIを設置するディレクトリ
     cgi_directories = ["/cgi-bin"]
 
+# ポート番号
 PORT = 8080
+
+# IPアドレス
 HOST = "127.0.0.1"
 
 print("http://localhost:8080/cgi-bin/sql.py")
-
 print("http://localhost:8080/cgi-bin/sql.py?id=A001")
 
+# サーバの開始
 httpd = HTTPServer((HOST, PORT), Handler)
 httpd.serve_forever()
 
